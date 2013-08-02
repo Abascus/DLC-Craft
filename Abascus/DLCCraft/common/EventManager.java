53ad7eab853b5548f9afd6f5efdaf77257aaeea7
Abascus <jannik.moell@web.de>
2013-08-02 12:15:14 +0200

    Whatever

--- Abascus/DLCCraft/common/EventManager.java
+++ Abascus/DLCCraft/common/EventManager.java
@@ -7,12 +7,13 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraftforge.event.ForgeSubscribe;
 import net.minecraftforge.event.entity.EntityEvent.EntityConstructing;
+import net.minecraftforge.event.entity.item.ItemEvent;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 
 public class EventManager 
 {
-	
+
 	@ForgeSubscribe
 	public void playerInteract(PlayerInteractEvent event)
 	{
@@ -28,58 +29,65 @@ public class EventManager
 		{
 			leftClickBlock(event);
 		}
-		
+
 	}
 	@ForgeSubscribe
 	public void entityConstructing(EntityConstructing event)
 	{
 		if(event.entity instanceof EntityPlayer)
 		{
+			PlayerDLCStats stats = new PlayerDLCStats();
+			EntityPlayer ep = (EntityPlayer)event.entity;
 			NBTTagCompound tags = event.entity.getEntityData();
-			if (!tags.hasKey("DLCCraft"))
+			tags.setCompoundTag("DLCCraft", new NBTTagCompound());
+			NBTTagList tagList = new NBTTagList();
+			NBTTagCompound dlc;
+
+			for (int i = 0; i < DLCManager.names.length; ++i)
 			{
-				tags.setCompoundTag("DLCCraft", new NBTTagCompound());
-				NBTTagList tagList = new NBTTagList();
-				NBTTagCompound dlc;
-
-		        for (int i = 0; i < DLCManager.names.length; ++i)
-		        {
-		            if (DLCManager.names[i] != null)
-		            {
-		                dlc = new NBTTagCompound();
-		                dlc.setInteger(DLCManager.names[i], (byte) 0);
-		                tagList.appendTag(dlc);
-		            }
-		        }
-
-		        tags.setTag("DLCCraft", tagList);
+				if (DLCManager.names[i] != null)
+				{
+					dlc = new NBTTagCompound();
+					dlc.setInteger(DLCManager.names[i], (byte) 0);
+					tagList.appendTag(dlc);
+				}
 			}
+
+			tags.setTag("DLCCraft", tagList);
+			stats.readFromNBT(tags);
+			DLCCraft.playerTracker.playerStats.put(ep.username, stats);
 		}
 	}
-	
-	
+
 	@ForgeSubscribe
-    public void onLivingDrop (LivingDropsEvent event)
-    {
+	public void itemEvent(ItemEvent event)
+	{
+
+	}
+
+
+	@ForgeSubscribe
+	public void onLivingDrop (LivingDropsEvent event)
+	{
 		try
 		{
 			EntityPlayer ep = (EntityPlayer)event.source.getSourceOfDamage();
-		PlayerDLCStats stats = DLCCraft.playerTracker.getPlayerDLCStats(ep.username);
-		State s = stats.states.get("mobDrops");
-		if(s.state != 2)
-		{
-			event.setCanceled(true);
-		}
+			PlayerDLCStats stats = DLCCraft.playerTracker.getPlayerDLCStats(ep.username);
+			State s = stats.states.get("mobDrops");
+			if(s.state != 2)
+			{
+				event.setCanceled(true);
+			}
 		}
 		catch(Exception e){}
-    }
-	
+	}
+
 	public void rightClickBlock(PlayerInteractEvent event)
 	{
 		PlayerDLCStats stats = DLCCraft.playerTracker.getPlayerDLCStats(event.entityPlayer.username);
-		
+
 	}
-	
+
 	public void rightClickAir(PlayerInteractEvent event)
 	{
 		PlayerDLCStats stats = DLCCraft.playerTracker.getPlayerDLCStats(event.entityPlayer.username);
@@ -92,13 +100,13 @@ public class EventManager
 			}
 		}
 	}
-	
+
 	public void leftClickBlock(PlayerInteractEvent event)
 	{
 		/*PlayerDLCStats stats = DLCCraft.playerTracker.getPlayerDLCStats(event.entityPlayer.username);
 		if(event.entityPlayer.worldObj.getBlockId(event.x, event.y, event.z) == Block.wood.blockID)
 		{
-			
+
 		if(stats.states.get("punchWood").state != 2)
 		{
 			event.setCanceled(true);
