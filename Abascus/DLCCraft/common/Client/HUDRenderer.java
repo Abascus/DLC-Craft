e79b2b6937c677745176f9ae84cbdcab65e3f6b4
Abascus <jannik.moell@web.de>
2013-08-02 20:46:59 +0200

    Whatever

--- Abascus/DLCCraft/common/Client/HUDRenderer.java
+++ Abascus/DLCCraft/common/Client/HUDRenderer.java
@@ -25,14 +25,6 @@ public class HUDRenderer extends Gui
 		// We need this to invoke the render engine.
 		this.mc = mc;
 	}
-
-	private static final int BUFF_ICON_SIZE = 18;
-	private static final int BUFF_ICON_SPACING = BUFF_ICON_SIZE + 2; // 2 pixels between buff icons
-	private static final int BUFF_ICON_BASE_U_OFFSET = 0;
-	private static final int BUFF_ICON_BASE_V_OFFSET = 198;
-	private static final int BUFF_ICONS_PER_ROW = 8;
-	protected static final ResourceLocation resourceLocation = new ResourceLocation("dlc craft/textures/items/Coin.png");
-	
 	@ForgeSubscribe
 	public void postRenderOverlay(RenderGameOverlayEvent.Post event)
 	{
@@ -43,27 +35,17 @@ public class HUDRenderer extends Gui
 			ScaledResolution scaledresolution = new ScaledResolution(this.mc.gameSettings, this.mc.displayWidth, this.mc.displayHeight);
 			int width = scaledresolution.getScaledWidth();
 			int height = scaledresolution.getScaledHeight();
+				
 
-			if (!this.mc.playerController.enableEverythingIsScrewedUpMode()) {
-				GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-
-				this.mc.renderEngine.func_110577_a(resourceLocation);
+				this.mc.renderEngine.func_110577_a(new ResourceLocation("dlc craft/textures/items/Coin.png"));
 				this.zLevel = -90.0F;
 
 				GL11.glEnable(GL12.GL_RESCALE_NORMAL);
 				GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 
 				drawTexturedModelRectFromIcon((int)(width / 1.4), height - 40, DLCCraft.instance.coin.getItemIcon(), 32, 32);
-				drawString(mc.fontRenderer, stats.Coins + "", (int)(width / 1.4)+40, height - 20, height - 40);
-
-
-
-
-
-
-			}
-
-
+				GL11.glColor4f(0.0F, 0.0F, 0.0F, 1.0F);
+				drawString(mc.fontRenderer, stats.Coins + "", (int)(width / 1.4)+40, height - 20, 9);
 
 		}
 	}
