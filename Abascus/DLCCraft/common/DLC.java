43cd043e0b3a6c2e9685cee6a423bf7647b0c711
Abascus <jannik.moell@web.de>
2013-08-02 12:39:13 +0200

    Whatever

---
+++ Abascus/DLCCraft/common/DLC.java
@@ -0,0 +1,15 @@
+package Abascus.DLCCraft.common;
+
+public class DLC 
+{
+	public int id;
+	public String name;
+	public int[] depend;
+	
+	public DLC(int i, String s)
+	{
+		id = i;
+		name = s;
+	}
+
+}
