5056e22014f7653db510bcf0744367607a042e5d
Abascus <jannik.moell@web.de>
2013-08-03 16:54:14 +0200

    Pause

--- Abascus/DLCCraft/common/GuiDLCSlot.java
+++ Abascus/DLCCraft/common/GuiDLCSlot.java
@@ -19,100 +19,101 @@ import cpw.mods.fml.relauncher.SideOnly;
 @SideOnly(Side.CLIENT)
 public class GuiDLCSlot extends GuiSlot
 {
-    final DLCShopGUI parentDLCGui;
+	final DLCShopGUI parentDLCGui;
 
-    public GuiDLCSlot(DLCShopGUI par1DLCShopGUI)
-    {
-        super(Minecraft.getMinecraft(), par1DLCShopGUI.width, par1DLCShopGUI.height, 32, par1DLCShopGUI.height - 64, 36);
-        this.parentDLCGui = par1DLCShopGUI;
-    }
+	public GuiDLCSlot(DLCShopGUI par1DLCShopGUI)
+	{
+		super(Minecraft.getMinecraft(), par1DLCShopGUI.width, par1DLCShopGUI.height, 32, par1DLCShopGUI.height - 64, 36);
+		this.parentDLCGui = par1DLCShopGUI;
+	}
 
-    /**
-     * Gets the size of the current slot list.
-     */
-    protected int getSize()
-    {
-        return DLCShopGUI.getSize(this.parentDLCGui).size();
-    }
+	/**
+	 * Gets the size of the current slot list.
+	 */
+	protected int getSize()
+	{
+		return DLCShopGUI.getSize(this.parentDLCGui).size();
+	}
 
-    /**
-     * the element in the slot that was clicked, boolean for wether it was double clicked or not
-     */
-    protected void elementClicked(int par1, boolean par2)
-    {
-        DLCShopGUI.onElementSelected(this.parentDLCGui, par1);
-        boolean flag1 = DLCShopGUI.getSelectedWorld(this.parentDLCGui) >= 0 && DLCShopGUI.getSelectedWorld(this.parentDLCGui) < this.getSize();
-        DLCShopGUI.getSelectButton(this.parentDLCGui).enabled = flag1;
+	/**
+	 * the element in the slot that was clicked, boolean for wether it was double clicked or not
+	 */
+	protected void elementClicked(int par1, boolean par2)
+	{
+		DLCShopGUI.onElementSelected(this.parentDLCGui, par1);
+		boolean flag1 = DLCShopGUI.getSelectedWorld(this.parentDLCGui) >= 0 && DLCShopGUI.getSelectedWorld(this.parentDLCGui) < this.getSize();
+		DLCShopGUI.getSelectButton(this.parentDLCGui).enabled = flag1;
 
-        if (par2 && flag1)
-        {
-           this.parentDLCGui.selectDLC(par1);
-        }
-    }
-    
-    
+		if (par2 && flag1)
+		{
+			this.parentDLCGui.selectDLC(par1);
+		}
+	}
 
-    /**
-     * returns true if the element passed in is currently selected
-     */
-    protected boolean isSelected(int par1)
-    {
-        return par1 == DLCShopGUI.getSelectedWorld(this.parentDLCGui);
-    }
 
-    /**
-     * return the height of the content being scrolled
-     */
-    protected int getContentHeight()
-    {
-        return DLCShopGUI.getSize(this.parentDLCGui).size() * 36*2;
-    }
 
-    protected void drawBackground()
-    {
-      //  this.parentDLCGui.drawDefaultBackground();
-    }
+	/**
+	 * returns true if the element passed in is currently selected
+	 */
+	protected boolean isSelected(int par1)
+	{
+		return par1 == DLCShopGUI.getSelectedWorld(this.parentDLCGui);
+	}
 
-    protected void drawSlot(int par1, int par2, int par3, int par4, Tessellator par5Tessellator)
-    {
-    PlayerDLCStats stats = DLCCraft.playerTracker.getPlayerDLCStats(parentDLCGui.ep.username);
-        DLC dlc = (DLC)DLCShopGUI.getSize(this.parentDLCGui).get(par1);
-        String s = parentDLCGui.getDesciption(par1);
-        String s1 = parentDLCGui.getSaveFileName(par1);
-        String s2 = "Cost: " + parentDLCGui.getCost(par1);
-        String s3 = "Depends on: " + stats.dlcManager.Names[parentDLCGui.getDepend(par1)];
-        
-        
-			int slot;
-			boolean c = false;
-			if(parentDLCGui.buy)
+	/**
+	 * return the height of the content being scrolled
+	 */
+	protected int getContentHeight()
+	{
+		return DLCShopGUI.getSize(this.parentDLCGui).size() * 36;
+	}
+
+	protected void drawBackground()
+	{
+		//  this.parentDLCGui.drawDefaultBackground();
+	}
+
+	protected void drawSlot(int par1, int par2, int par3, int par4, Tessellator par5Tessellator)
+	{
+		PlayerDLCStats stats = DLCCraft.playerTracker.getPlayerDLCStats(parentDLCGui.ep.username);
+		DLC dlc = (DLC)DLCShopGUI.getSize(this.parentDLCGui).get(par1);
+		String s = parentDLCGui.getDesciption(par1);
+		String s1 = parentDLCGui.getSaveFileName(par1);
+		String s2 = "Cost: " + parentDLCGui.getCost(par1);
+
+
+
+		int slot;
+		boolean c = false;
+		if(parentDLCGui.buy)
+		{
+			if(stats.Coins >= stats.dlcManager.cost[dlc.id])
 			{
-				if(stats.Coins >= stats.dlcManager.cost[dlc.id])
-				{
-					c = true;
-				}
+				c = true;
 			}
+		}
 
-        this.parentDLCGui.drawString(Minecraft.getMinecraft().fontRenderer, s, par2 + 2, par3 + 1, 16777215);
-        this.parentDLCGui.drawString(Minecraft.getMinecraft().fontRenderer, s1, par2 + 2, par3 + 12, 8421504);
-        if(parentDLCGui.buy)
-        {
-        if(c)
-        {
-        	 this.parentDLCGui.drawString(Minecraft.getMinecraft().fontRenderer, s2, par2 + 2, par3 + 24, 2421504);
-        }
-        else
-        {
-        this.parentDLCGui.drawString(Minecraft.getMinecraft().fontRenderer, s2, par2 + 2, par3 + 24, 11421504);
-        }
-        }
-        else
-        {
-        	 this.parentDLCGui.drawString(Minecraft.getMinecraft().fontRenderer, s2, par2 + 2, par3 + 24, 21421504);
-        }
-        if(parentDLCGui.getDepend(par1) != -1)
-        {
-        this.parentDLCGui.drawString(Minecraft.getMinecraft().fontRenderer, s3, par2 + 2, par3 + 36, 8421504);
-        }
-    }
+		this.parentDLCGui.drawString(Minecraft.getMinecraft().fontRenderer, s, par2 + 2, par3 + 1, 16777215);
+		this.parentDLCGui.drawString(Minecraft.getMinecraft().fontRenderer, s1, par2 + 2, par3 + 12, 8421504);
+		if(parentDLCGui.buy)
+		{
+			if(c)
+			{
+				this.parentDLCGui.drawString(Minecraft.getMinecraft().fontRenderer, s2, par2 + 2, par3 + 24, 2421504);
+			}
+			else
+			{
+				this.parentDLCGui.drawString(Minecraft.getMinecraft().fontRenderer, s2, par2 + 2, par3 + 24, 11421504);
+			}
+		}
+		else
+		{
+			this.parentDLCGui.drawString(Minecraft.getMinecraft().fontRenderer, s2, par2 + 2, par3 + 24, 21421504);
+		}
+		if(parentDLCGui.getDepend(par1) != -1)
+		{
+			String s3 = "Depends on: " + stats.dlcManager.Names[parentDLCGui.getDepend(par1)];
+			this.parentDLCGui.drawString(Minecraft.getMinecraft().fontRenderer, s3, par2 + 2, par3 + 36, 8421504);
+		}
+	}
 }
