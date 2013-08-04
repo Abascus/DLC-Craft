fcee097cc123b832cf46f0fb7342c8713b5ef505
Abascus <jannik.moell@web.de>
2013-08-04 17:36:41 +0200

    Whatever

--- Abascus/DLCCraft/common/DLCCraftItem.java
+++ Abascus/DLCCraft/common/DLCCraftItem.java
@@ -1,8 +1,11 @@
 package Abascus.DLCCraft.common;
 
 import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Icon;
+import net.minecraft.world.World;
 
 public class DLCCraftItem extends Item
 {
@@ -31,6 +34,15 @@ public class DLCCraftItem extends Item
 			itemIcon = par1IconRegister.registerIcon("DLC Craft:shop");
 		}
 	}
+	
+	public ItemStack onItemRightClick(ItemStack par1ItemStack, World world, EntityPlayer player)
+    {
+		if(par1ItemStack.itemID == DLCCraft.instance.shop.itemID)
+		{
+			player.openGui(DLCCraft.instance, 0, world, player.posX, player.posY, player.posZ);
+		}
+		return par1ItemStack;
+    }
 	
 
 
