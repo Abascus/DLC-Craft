07a3329773e842880a2b93c48203e839593a3453
Abascus <jannik.moell@web.de>
2013-08-05 15:03:48 +0200

    Whatever

--- Abascus/DLCCraft/common/Client/RenderTickHandler.java
+++ Abascus/DLCCraft/common/Client/RenderTickHandler.java
@@ -34,7 +34,7 @@ public class RenderTickHandler implements ITickHandler
 	public ItemStack is = new ItemStack(DLCCraft.instance.coin, 1);
 	protected static RenderItem itemRenderer = new RenderItem();
 
-	private static final ResourceLocation rr = new ResourceLocation("dlc craft/items/coin.png");
+	private static final ResourceLocation rr = new ResourceLocation("dlc craft/items/Coin.png");
     
 	int zLevel;
 	@Override
@@ -53,33 +53,14 @@ public class RenderTickHandler implements ITickHandler
 				ScaledResolution scaledresolution = new ScaledResolution(Minecraft.getMinecraft().gameSettings, Minecraft.getMinecraft().displayWidth, Minecraft.getMinecraft().displayHeight);
 				int width = scaledresolution.getScaledWidth();
 				int height = scaledresolution.getScaledHeight();
-				GL11.glEnable(GL12.GL_RESCALE_NORMAL);
-				GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-
-				GL11.glTranslatef(0.0F, 0.0F, 32.0F);
 				
 				itemRenderer.renderItemAndEffectIntoGUI(Minecraft.getMinecraft().fontRenderer, Minecraft.getMinecraft().func_110434_K(), is, (int)(width / 1.3), 10);
-				Minecraft.getMinecraft().func_110434_K().func_110577_a(rr);
-				drawTexturedModalRect((int)(width / 1.3), 10, 0, 0, 64, 64);
 				Minecraft.getMinecraft().fontRenderer.drawString(stats.dlcManager.Coins + "", (int)(width / 1.3)+40, 14, 11421504, false);
 				}
 				}
 		}
 		catch(Exception e){}
 	}
-	
-	 public void drawTexturedModalRect(int par1, int par2, int par3, int par4, int par5, int par6)
-	    {
-	        float f = 0.00390625F;
-	        float f1 = 0.00390625F;
-	        Tessellator tessellator = Tessellator.instance;
-	        tessellator.startDrawingQuads();
-	        tessellator.addVertexWithUV((double)(par1 + 0), (double)(par2 + par6), (double)this.zLevel, (double)((float)(par3 + 0) * f), (double)((float)(par4 + par6) * f1));
-	        tessellator.addVertexWithUV((double)(par1 + par5), (double)(par2 + par6), (double)this.zLevel, (double)((float)(par3 + par5) * f), (double)((float)(par4 + par6) * f1));
-	        tessellator.addVertexWithUV((double)(par1 + par5), (double)(par2 + 0), (double)this.zLevel, (double)((float)(par3 + par5) * f), (double)((float)(par4 + 0) * f1));
-	        tessellator.addVertexWithUV((double)(par1 + 0), (double)(par2 + 0), (double)this.zLevel, (double)((float)(par3 + 0) * f), (double)((float)(par4 + 0) * f1));
-	        tessellator.draw();
-	    }
 
 	@Override
 	public EnumSet<TickType> ticks()
