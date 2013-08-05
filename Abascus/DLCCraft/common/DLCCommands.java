296d4ace96bc43b7e8206a6897ec8f4eedde3efe
Abascus <jannik.moell@web.de>
2013-08-05 13:28:08 +0200

    Whatever

--- Abascus/DLCCraft/common/DLCCommands.java
+++ Abascus/DLCCraft/common/DLCCommands.java
@@ -31,6 +31,30 @@ public class DLCCommands extends CommandBase {
     {
 		return Arrays.asList(new String[] {"DLC"});
     }
+	
+	public void process(int i1, String[] astring)
+	{
+		try
+		{
+		PlayerDLCStats stats = DLCCraft.playerTracker.getPlayerDLCStats(astring[0]);
+		int i = stats.dlcManager.getID(astring[2]);
+		stats.dlcManager.dlcs[i].state = i1;
+		DLCCraft.playerTracker.playerStats.put(astring[0], stats);
+		Side side = FMLCommonHandler.instance().getEffectiveSide();
+		if (side == Side.SERVER)
+		{
+			DLCCraft.playerTracker.sendDLCs2(stats.player.get(), stats);
+		}
+		else
+		{
+			DLCCraft.playerTracker.sendDLCs(stats.player.get(), stats);
+		}
+		}
+		catch(Exception e)
+		{
+			throw new WrongUsageException(getUsageString(), new Object[0]);
+		}
+	}
 
 	@Override
 	public void processCommand(ICommandSender icommandsender, String[] astring) 
@@ -43,11 +67,23 @@ public class DLCCommands extends CommandBase {
 			{
 				if("delet".startsWith(astring[1].toLowerCase()))
 				{
+					process(0, astring);
+					
+				}
+				else if("unlock".startsWith(astring[1].toLowerCase()))
+				{
+					process(1, astring);
+				}
+				else if("buy".startsWith(astring[1].toLowerCase()))
+				{
+					process(2, astring);
+				}
+				else if("coinsadd".startsWith(astring[1].toLowerCase()))
+				{
 					try
 					{
 					PlayerDLCStats stats = DLCCraft.playerTracker.getPlayerDLCStats(astring[0]);
-					int i = stats.dlcManager.getID(astring[2]);
-					stats.dlcManager.dlcs[i].state = 0;
+					stats.dlcManager.Coins+= Integer.parseInt(astring[2]);
 					DLCCraft.playerTracker.playerStats.put(astring[0], stats);
 					Side side = FMLCommonHandler.instance().getEffectiveSide();
 					if (side == Side.SERVER)
@@ -79,7 +115,7 @@ public class DLCCommands extends CommandBase {
 				"/DLC <player> delet <dlc name>       Set the State of an DLC to Unavailible.\n" +
 				"/DLC <player> unlock <dlc name>      Set the State of an DLC to Availible.\n" +
 				"/DLC <player> buy  <dlc name>        Set the State of an DLC to Bought. \n"+
-				"/DLC <player> CoinsAdd  <amount>     Gives player Coins. \n";
+				"/DLC <player> coinsadd  <amount>     Gives player Coins. \n";
 	}
 
 }
